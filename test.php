32becb718acb1059642e8cbf37800826a69b9268
rakibkamal98 <rk6sp@virgnia.edu>
2018-03-18 14:22:12 -0400

    Changed buttons

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+require '../../vendor/autoload.php';
+require '../../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require '../../vendor/phpmailer/phpmailer/src/SMTP.php';
+require '../../vendor/phpmailer/phpmailer/src/Exception.php';
+$mail = new PHPMailer;
+$mail->setFrom('from@example.com', 'Your Name');
+$mail->addAddress('myfriend@example.net', 'My Friend');
+$mail->Subject  = 'First PHPMailer Message';
+$mail->Body     = 'Hi! This is my first e-mail sent through PHPMailer.';
+if(!$mail->send()) {
+  echo 'Message was not sent.';
+  echo 'Mailer error: ' . $mail->ErrorInfo;
+} else {
+  echo 'Message has been sent.';
+}
